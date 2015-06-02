b5023ce0d1f9dab831b31408e50e0eb9e690c997
Nick Delitski <ndelitski@gmail.com>
2015-06-02 18:19:57 +0300

    babel-core to main deps

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,18 @@ container=locale
 
 
 # npm run build
+rm -rf ./build
 mkdir -p ./build
 git archive HEAD | tar -x -C build
 cp -r ./dist ./build
 cd ./build
+npm i --production
 docker build --rm -t $image ./
-docker kill $container
-docker rm $container
+# docker kill $container &>/dev/null
+# docker rm $container &>/dev/null
 docker run \
-  -d -p 3005:80 \
-  -e PORT=80
+  -d -p 3060:80 \
+  -e PORT=80 \
   -e DEBUG=synccloud* \
   --name $container $image
 cd -  
