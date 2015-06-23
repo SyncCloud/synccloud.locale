ac10d1a47a6c84e98d5ffd740581bd3a8c8ebf41
Nick Delitski <ndelitski@gmail.com>
2015-06-23 16:33:56 +0300

    log save activity model;

--- app/index.js
+++ app/index.js
@@ -1,6 +1,3 @@
-//see https://github.com/babel/babel-loader#custom-polyfills-eg-promise-library
-require('babel-runtime/core-js/promise').default = require('bluebird');
-
 require('../common/globals');
 
 // Start application
